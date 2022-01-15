8fcab8ba769103225c86e2e0d0f25e244cb530e0
jysong999 <jysonggm@gmail.com>
2022-01-16 03:05:30 +0900

    operator

---
+++ HELLOWORLD/c++/HelloWorld/#define.cpp
@@ -0,0 +1,19 @@
+// #define 이름 대체값
+
+#include <iostream>
+using namespace std;
+
+#define pi 3.14
+#define newline '\n'
+
+int main ()
+{
+    double r = 5;
+    double circle;
+
+    circle = 2*pi*r;
+    cout << circle;
+    cout << newline;
+
+
+}
