7cf874f05f986a534e59361c83af1cb81d64c8e2
jysong999 <jysonggm@gmail.com>
2022-01-16 02:45:11 +0900

    constant value

---
+++ HELLOWORLD/c++/TypedConstant.cpp
@@ -0,0 +1,18 @@
+// constant value
+
+#include <iostream>
+using namespace std;
+
+const double pi = 3.14;
+const char newline = '\n';
+
+int main ()
+{
+    double r = 0.5;
+    double circle;
+
+    circle = 2 * pi * r;
+
+    cout << circle;
+    cout << newline;
+}
