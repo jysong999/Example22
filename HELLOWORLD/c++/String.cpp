5cc9cd5269dd4c1322ea8b4856c600cd60493df8
jysong999 <jysonggm@gmail.com>
2022-01-16 01:54:58 +0900

    string 변수

---
+++ HELLOWORLD/c++/String.cpp
@@ -0,0 +1,14 @@
+// my first string
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string mystring;
+    mystring = "This is my string";
+    cout << mystring;
+
+    return 0;
+}
