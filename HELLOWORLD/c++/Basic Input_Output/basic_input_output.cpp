4a7fd37bb194f6b85b6cf3658976816d673e66f0
jysong999 <jysonggm@gmail.com>
2022-01-20 22:26:38 +0900

    cout, cin

--- HELLOWORLD/c++/Basic Input_Output/basic_input_output.cpp
+++ HELLOWORLD/c++/Basic Input_Output/basic_input_output.cpp
@@ -1,9 +1,31 @@
 // cout : standard output
 
 #include <iostream>
+#include <windows.h>
+
 using namespace std;
+// int main()
+// { 
+//     cout << "output sentence";
+
+//     //chaining
+//     cout << "This " << "is a " << "single C++ statement\n";
+
+//     int age ;
+//     age = 20 ; 
+//     cout << "I am " << age << " years old\n " ;
+//     cout << "or I can use 'endl'" << endl;
+// } 
+
+
+// cin : standard input
+
 int main()
-{ 
-    cout << "output sentence";
-    
+{   
+    int age;
+    cout << "enter your age: \n";
+    cin >> age;
+    cout << "your age is " << age;
+    Sleep(1000);
+    return 0;
 }
