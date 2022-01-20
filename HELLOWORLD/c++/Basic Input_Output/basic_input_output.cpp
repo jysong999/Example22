ea8caf7870171f2bd5757470ea1a417aacdbbb06
jysong999 <jysonggm@gmail.com>
2022-01-20 22:37:52 +0900

    cin and strings

--- HELLOWORLD/c++/Basic Input_Output/basic_input_output.cpp
+++ HELLOWORLD/c++/Basic Input_Output/basic_input_output.cpp
@@ -1,7 +1,9 @@
 // cout : standard output
 
 #include <iostream>
-#include <windows.h>
+#include <windows.h> // for Sleep()
+#include <string>
+
 
 using namespace std;
 // int main()
@@ -20,12 +22,33 @@ using namespace std;
 
 // cin : standard input
 
+// int main()
+// {   
+//     int age;
+//     cout << "enter your age: \n";
+//     cin >> age;
+//     cout << "your age is " << age;
+//     Sleep(1000);
+//     return 0;
+// }
+
+
+// cin and strings - getline ; extracting a string = 한 단어만 extract. cin으로 문장 받아들이려면 getlint() 함수 써야함.
+// getline(cin, 저장할 변수)
+// #include<string>
+
 int main()
-{   
-    int age;
-    cout << "enter your age: \n";
-    cin >> age;
-    cout << "your age is " << age;
+{
+    string mystr;
+    cout << "What's your name? \n" ;
+    getline(cin, mystr) ;
+    cout << "HEllo " << mystr << ".\n" ;
+    cout << "What's your favorite team?" ;
+    getline(cin, mystr) ;
+    cout << "I like " << mystr << "too! \n" ;
     Sleep(1000);
+
+
     return 0;
+
 }
