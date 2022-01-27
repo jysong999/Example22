4b367e7f9e56ada0fb22b299f6487220bb183fde
jysong999 <jysonggm@gmail.com>
2022-01-27 16:58:54 +0900

    ex0106

---
+++ HELLOWORLD/c/ch.1/6.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+// int main(void)
+// {
+
+//     int x;
+//     x = 15;
+
+//     printf("x의 값은 %d입니다.\n", x);
+
+//     return 0;
+
+// }
+
+int main(void)
+{
+    int x  = 15;
+    printf("x의 값은 %d입니다.\n", x);
+
+    return 0;
+}
